64fb148bc1688bad6f5a16ce3171778967d5166b
cetusian <madalintat@gmail.com>
2026-04-22 01:47:44 +0300

    fix: explicit ctrl+hjkl keymaps for vim-tmux-navigator

--- lua/cetusian/plugins/init.lua
+++ lua/cetusian/plugins/init.lua
@@ -1,4 +1,19 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "nvim-lua/plenary.nvim",
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>",  desc = "Navigate left (split/tmux)" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>",  desc = "Navigate down (split/tmux)" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>",    desc = "Navigate up (split/tmux)" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate right (split/tmux)" },
+    },
+  },
 }
