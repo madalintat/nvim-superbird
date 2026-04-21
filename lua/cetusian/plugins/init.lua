1b9e858791c32636819089f4a8b5f8ee43637797
cetusian <madalintat@gmail.com>
2026-04-22 01:50:19 +0300

    fix: eager-load vim-tmux-navigator with globals set in init

--- lua/cetusian/plugins/init.lua
+++ lua/cetusian/plugins/init.lua
@@ -2,18 +2,16 @@ return {
   "nvim-lua/plenary.nvim",
   {
     "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>",  desc = "Navigate left (split/tmux)" },
-      { "<C-j>", "<cmd>TmuxNavigateDown<CR>",  desc = "Navigate down (split/tmux)" },
-      { "<C-k>", "<cmd>TmuxNavigateUp<CR>",    desc = "Navigate up (split/tmux)" },
-      { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate right (split/tmux)" },
-    },
+    lazy = false,
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_disable_netrw_workaround = 1
+    end,
+    config = function()
+      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>",  { silent = true, desc = "Navigate left" })
+      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>",  { silent = true, desc = "Navigate down" })
+      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>",    { silent = true, desc = "Navigate up" })
+      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { silent = true, desc = "Navigate right" })
+    end,
   },
 }
